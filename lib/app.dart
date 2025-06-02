7908b36918ddda7ac9ea69d8517ae8e592a15e8e
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2025-06-02 19:58:30 +0100

    Initialize English language JSON file for localization

--- lib/app.dart
+++ lib/app.dart
@@ -1,34 +1,72 @@
-import 'package:flutter/foundation.dart';
+import 'package:app/core/localization/localization_cubit.dart';
+import 'package:app/core/routing/routers/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'core/flavors/flavors.dart';
-import 'pages/my_home_page.dart';
+import 'core/di/locator.dart';
+import 'core/localization/app_localization.dart';
+import 'gen/fonts.gen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: F.title,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: _flavorBanner(child: MyHomePage(), show: kDebugMode),
+    return ScreenUtilInit(
+      designSize: const Size(1440, 1024),
+      builder: (_, __) {
+        return BlocProvider(
+          create: (context) => locator<LocalizationCubit>(),
+          child: _MaterialApp(),
+        );
+      },
     );
   }
+}
 
-  Widget _flavorBanner({required Widget child, bool show = true}) =>
-      show
-      ? Banner(
-          location: BannerLocation.bottomEnd,
-          message: F.name,
-          color: Colors.green.withAlpha(150),
-          textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0,
-          ),
-          textDirection: TextDirection.ltr,
-          child: child,
-        )
-      : Container(child: child);
+class _MaterialApp extends StatelessWidget {
+  const _MaterialApp();
+
+  @override
+  Widget build(BuildContext context) {
+    final router = locator<AppRouter>();
+
+    final local = context.watch<LocalizationCubit>().state;
+
+    return MaterialApp.router(
+      routerConfig: router.router,
+      theme: ThemeData(fontFamily: FontFamily.poppins),
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('fr'), // French
+        Locale('ar'), // Arabic
+      ],
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: local,
+      localeResolutionCallback: (locale, supportedLocales) {
+        if (locale != null &&
+            supportedLocales.any(
+              (element) =>
+                  element.languageCode == locale.languageCode,
+            )) {
+          locator<LocalizationCubit>().changeLanguage(
+            locale.languageCode,
+          );
+        }
+        
+        // If the current device locale is not supported, use the first one
+        locator<LocalizationCubit>().changeLanguage(
+          supportedLocales.first.languageCode,
+        );
+        return null;
+      },
+    );
+  }
 }
--- lib/core/localization/localization_cubit.dart
+++ lib/core/localization/localization_cubit.dart
@@ -1,21 +1,23 @@
+import 'package:app/core/di/locator.dart';
+import 'package:app/core/services/cache/cache_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
 @lazySingleton
 class LocalizationCubit extends Cubit<Locale?> {
-  // final _cacheService = locator<CacheService>();
+  final _cacheService = locator<CacheService>();
   LocalizationCubit() : super(null) {
     init();
   }
 
   void init() {
-    // final langCode = _cacheService.getString('language') ?? 'en';
-    // emit(Locale(langCode));
+    final langCode = _cacheService.getString('language') ?? 'en';
+    emit(Locale(langCode));
   }
 
   void changeLanguage(String langCode) {
-    // _cacheService.setData('language', langCode);
+    _cacheService.setData('language', langCode);
     emit(Locale(langCode));
   }
 }
