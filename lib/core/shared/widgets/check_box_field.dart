6cb4b865259be421816e41b01cb3bb2a83beab05
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2025-06-02 19:44:08 +0100

    feat: add AppCheckBoxField widget and FormFieldError component; include material_symbols_icons dependency in pubspec.yaml

---
+++ lib/core/shared/widgets/check_box_field.dart
@@ -0,0 +1,73 @@
+import 'package:app/core/shared/editioncontollers/boolean_editigcontroller.dart';
+import 'package:app/core/shared/widgets/form_field_props.dart';
+import 'package:app/core/themes/font_styles.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppCheckBoxField extends StatelessWidget {
+  final BooleanEditingController controller;
+
+  final String? Function(bool?)? validator;
+
+  final String label;
+  final bool isRequired;
+
+  const AppCheckBoxField({
+    super.key,
+    required this.controller,
+    this.validator,
+    required this.label,
+    this.isRequired = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FormField(
+      validator: (_) => validator?.call(controller.value),
+      builder: (state) => Row(
+        spacing: 4.w,
+        children: [
+          ValueListenableBuilder(
+            valueListenable: controller,
+            builder: (_, value, __) {
+              return Checkbox(
+                value: value,
+                onChanged: (value) {
+                  controller.value = value ?? false;
+                },
+              );
+            },
+          ),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    text: label,
+                    style: AppTextStyles.medium,
+                    children: [
+                      if (isRequired)
+                        TextSpan(
+                          text: ' *',
+                          style: AppTextStyles.medium.copyWith(
+                            color: Colors.red,
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+
+                if (state.hasError) ...[
+                  FormFieldError(state.errorText!),
+                ],
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/core/shared/widgets/form_field_props.dart
@@ -0,0 +1,73 @@
+import 'package:app/core/themes/colors.dart';
+import 'package:app/core/themes/dimensions.dart';
+import 'package:app/core/themes/font_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class FormFieldError extends StatelessWidget {
+  final String errorText;
+  const FormFieldError(this.errorText, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        widthSpace(8),
+        Icon(Symbols.error_outline, color: AppColors.red, size: 20.r),
+        widthSpace(8),
+        Expanded(child: Text(errorText, style: AppTextStyles.error)),
+        widthSpace(8),
+      ],
+    );
+  }
+}
+
+class FormFieldLabel extends StatelessWidget {
+  final String label;
+  final bool isRequired;
+  final TextStyle style;
+
+  final VoidCallback? onAdd;
+
+  FormFieldLabel(
+    this.label, {
+    super.key,
+    this.isRequired = false,
+    TextStyle? style,
+    this.onAdd,
+  }) : style = style ?? AppTextStyles.large;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: RichText(
+            text: TextSpan(
+              text: label,
+              style: style,
+              children: [
+                if (isRequired)
+                  TextSpan(
+                    text: ' *',
+                    style: style.copyWith(color: Colors.red),
+                  ),
+              ],
+            ),
+          ),
+        ),
+        if (onAdd != null)
+          InkWell(
+            // Add button
+            onTap: onAdd,
+            child: Icon(
+              Symbols.add_circle_outline,
+              color: AppColors.black,
+            ),
+          ),
+      ],
+    );
+  }
+}
