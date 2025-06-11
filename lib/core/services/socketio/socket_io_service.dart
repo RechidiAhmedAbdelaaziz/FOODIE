93dc1716d5c195e8278004895cba8d10551ceef3
RechidiAhmedAbdelaaziz <aa.rechidi@esi-sba.dz>
2025-06-11 14:10:36 +0100

    feat(socketio): add SocketIoService for real-time communication and update environment configuration

---
+++ lib/core/services/socketio/socket_io_service.dart
@@ -0,0 +1,58 @@
+import 'package:app/core/di/locator.dart';
+import 'package:app/core/networking/api_response_model.dart';
+import 'package:app/features/auth/data/source/auth_cache.dart';
+import 'package:app/features/auth/logic/auth_cubit.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:injectable/injectable.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+@lazySingleton
+class SocketIoService {
+  late io.Socket _socket;
+
+  void connect() async {
+    if (_socket.connected) return;
+
+    await locator<AuthCubit>().refreshToken();
+
+    final url = dotenv.env['SOCKET_IO_URL'];
+    final token = locator<AuthCache>().accessToken;
+
+    _socket = io.io(url, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+      'query': {'token': token},
+    });
+
+    _connect();
+  }
+
+  void _connect() => _socket.connect();
+
+  void disconnect() => _socket.disconnect();
+
+  void onData(String event, Function(DataApiResponse data) callback) {
+    _socket.on(event, (data) {
+      if (data is Map<String, dynamic>) {
+        final response = DataApiResponse.fromJson(data);
+        callback(response);
+      } else {
+        callback(data);
+      }
+    });
+  }
+
+  void onMultiData(
+    String event,
+    Function(MultiDataApiResponse data) callback,
+  ) {
+    _socket.on(event, (data) {
+      if (data is Map<String, dynamic>) {
+        final response = MultiDataApiResponse.fromJson(data);
+        callback(response);
+      } else {
+        callback(data);
+      }
+    });
+  }
+}
